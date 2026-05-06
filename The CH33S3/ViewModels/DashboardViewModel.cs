74803d3c1dec702e7a3e2e946ed555ef7bbf23ce
Luis Cawa <cawaluis8@gmail.com>
2026-05-06 12:07:04 +0800

    i fixed the chessgame

--- The CH33S3/ViewModels/DashboardViewModel.cs
+++ The CH33S3/ViewModels/DashboardViewModel.cs
@@ -9,17 +9,27 @@ namespace The_CH33S3.ViewModels
 {
     internal class DashboardViewModel : BaseViewModel
     {
+        private BaseViewModel? _currentView;
         private ProfileViewModel? _profileViewModel;
 
+        public BaseViewModel? CurrentView
+        {
+            get => _currentView;
+            set => SetProperty(ref _currentView, value);
+        }
         public ProfileViewModel? ProfileViewModel
         {
             get => _profileViewModel;
             set => SetProperty(ref _profileViewModel, value);
         }
 
+
+
         public DashboardViewModel(UserModel user)
         {
             ProfileViewModel = new ProfileViewModel(user);
         }
+
+
     }
 }
